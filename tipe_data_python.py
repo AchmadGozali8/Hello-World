d77165a7eb1058fd5c3e46336e89d51d599fe7b4
AchmadGozali8 <achmadg6@gmail.com>
2015-12-22 14:51:39 +0800

    Push to github

---
+++ tipe_data_python.py
@@ -0,0 +1,58 @@
+#Variable dan Tipe Data di Python
+
+#variable
+var = "<== variable"
+var1 = 123
+var2 = 1.1245
+
+print var
+print var1
+print var2
+
+#tipe data
+
+#tipe data string
+string = "ini string"
+print string
+
+#tipe data integrer
+integrer = 12345
+print integrer
+
+#tipe data float
+_float=1.234
+print _float
+
+#tipe data long
+_long = 12345678910L
+print _long
+
+#tipe data list
+_list = ["ini",
+		"adalah",
+		"list"]
+print _list
+
+#tipe data tuple
+_tuple = ("ini", "adalah", "tuple")
+print _tuple
+#tipe data dictionary
+dictionary = {"Nama":"Kepo -,",
+			"Kerjaan":"hmmmm"}
+print dictionary
+
+#tipe data complex
+	#pasangan angka real dan imajiner
+	#misal 1 + 5j
+
+#tipe data date
+	#bilangan yang dikonversi menjadi format tanggal
+
+#tipe data hexadecimal
+	#bilangan dalam format hexa misal 7b, 4d2
+
+#tipe data booelan
+	#True dan False
+	#atau
+a = 1#True
+b = 0#False
